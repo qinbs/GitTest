1385bfc18dcab04fc37aa26a47013976e409cf95
qj <qj@example.com>
2016-04-26 14:29:58 +0800

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by CHEN    WEIXIN on 16/4/26.
+//  Copyright (c) 2016年 xinguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by CHEN    WEIXIN on 16/4/26.
+//  Copyright (c) 2016年 xinguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
